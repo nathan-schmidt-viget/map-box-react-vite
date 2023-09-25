7676a9b625aeead331b7aed9d7c929daef5e6779
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2023-09-25 07:16:05 -0600

    Adding TW into project

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,15 @@ import viteLogo from "/vite.svg";
 function App() {
   return (
     <>
-      <h1>National Park Service Locations</h1>
+      <div className="py-8 max-w-4xl mx-auto text-center px-4">
+        <h1 className="text-6xl">National Park Service Locations</h1>
+        <p className="text-md mt-8">
+          The National Park Service manages and preserves over 400 stunning locations across the United States, ranging from iconic natural wonders like the Grand Canyon to historic sites such as Independence Hall.
+        </p>
+        <p className="text-md mt-4">
+          This exploration looks into build a better location search tool using <a className="underline hover:no-underline" href="https://www.mapbox.com/">MapBox</a>. 
+        </p>
+      </div>
       <Map />
       <footer className="footer">
         <p>Built on Vite and React</p>
--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -8,30 +8,30 @@ const LocationDetails = ({ geoMapItem, isLoading }) => {
     return null;
   }
   return (
-    <article>
+    <article className="px-4 mt-8 max-w-6xl mx-auto w-full basis-1/2">
       {isLoading && <p className="loading">Loading...</p>}
       {!isLoading && geoMapItem.data.map((location) => (
-        <div key={location.id} className="location">
-          <div className="location-info">
-            <h2>{location.fullName}</h2>
-            <p>{location.description}</p>
+        <div key={location.id} >
+          <div className="md:col-span-2">
+            <h2 className="text-4xl">{location.fullName}</h2>
+            <p className="mt-2">{location.description}</p>
             {location.entranceFees.length > 0 &&
-              <details>
-                <h3>Entrance Fees</h3>
-                <div className="location-fee">
+              <details className="py-2">
+                <summary className="bg-zinc-100 px-1.5 py-2 text-zinc-900 rounded-md">Entrance Fees</summary>
+                <div className="mt-3 grid gap-8">
                   {location.entranceFees.map((fee, index) => (
                     <div key={index}>
-                      <h3>{fee.title} - ${fee.cost}</h3>
-                      <p>{fee.description}</p>
+                      <h3 className="text-lg">{fee.title} - ${fee.cost}</h3>
+                      <p className="mt-1">{fee.description}</p>
                     </div>
                   ))}
                 </div>
               </details>
             }
 
-            <details>
-              <summary>Images</summary>
-              <div className="location-images">
+            <details className="py-2">
+              <summary className="bg-zinc-100 px-1.5 py-2 text-zinc-900 rounded-md">Images</summary>
+              <div className="mt-3">
                 {location.images.map((image, index) => (
                   <figure key={index}>
                     <img src={image.url} alt={image.altText} />
@@ -41,17 +41,18 @@ const LocationDetails = ({ geoMapItem, isLoading }) => {
               </div>
             </details>
 
-            <details>
-              <summary>Weather Info</summary>
-              <div className="location-weather">
+            <details className="py-2">
+            <summary className="bg-zinc-100 px-1.5 py-2 text-zinc-900 rounded-md">Weather Info</summary>
+              <div className="mt-3">
                 <p>{location.weatherInfo}</p>
               </div>
             </details>
           </div>
-          <aside className="location-sidebar">
-            <h3>Operating Hours</h3>
-            <p>{location.operatingHours[0].description}</p>
-            <ul>
+
+          <aside>
+            <h3 className="text-3xl">Operating Hours</h3>
+            <p className="mt-2 text-sm">{location.operatingHours[0].description}</p>
+            <ul className="mt-4 flex gap-1 flex-col text-sm">
               <li>Sunday - {location.operatingHours[0].standardHours.sunday}</li>
               <li>Monday - {location.operatingHours[0].standardHours.monday}</li>
               <li>Tuesday - {location.operatingHours[0].standardHours.tuesday}</li>
@@ -66,15 +67,15 @@ const LocationDetails = ({ geoMapItem, isLoading }) => {
                 Saturday - {location.operatingHours[0].standardHours.saturday}
               </li>
             </ul>
-            <h3>Phone Number(s)</h3>
-            <ul>
+            <h3 className="text-xl mt-8">Phone Number(s)</h3>
+            <ul className="mt-2 flex gap-1 flex-col text-sm">
               {location.contacts.phoneNumbers.map((phone, index) => (
                 <li key={index}>
                   {phone.type}: <a href={`tel:${phone.phoneNumber}`}>{formatPhoneNumber(phone.phoneNumber)}</a>
                 </li>
               ))}
             </ul>
-            <h3>Address</h3>
+            <h3 className="text-xl mt-8">Address</h3>
             <p>
               {location.addresses[0].line1}
               <br></br>
